83e0e083b7d801e609dce17d0fa1995a8c6206dd
PrettyKawaii <litvinocih@gmail.com>
2025-09-19 01:15:49 +0300

    1/6 part of lr1!

--- subjects/script-languages/lr1/variant-12/lr1-12.py
+++ subjects/script-languages/lr1/variant-12/lr1-12.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+import math as m
+
+''' 
+1) Сделайте так, чтобы число секунд отображалось в
+виде дни:часы:минуты:секунды. Ввод закончить, когда пользователь
+ввел 0. – 2 балла
+'''
+isFinished = False
+while not isFinished:
+    try:
+        seconds = int(input("Введите целое количество секунд (0 - выход): "))
+        if seconds == 0:
+            isFinished = True
+            print("Программа завершена.")
+        else:
+            days = seconds // (24*3600)
+            seconds_left = seconds % (24*3600) 
+
+            hours = seconds_left // 3600
+            seconds_left %= 3600
+
+            minutes = seconds_left // 60
+            seconds_left %= 60
+            print("Результат - ", end='')
+            #print(days, hours, minutes, seconds_left, sep=':')
+            print(f"{days:02d}:{hours:02d}:{minutes:02d}:{seconds_left:02d}")
+
+    except (ValueError):
+        print("Ошибка ввода! Потворите попытку.")
+
